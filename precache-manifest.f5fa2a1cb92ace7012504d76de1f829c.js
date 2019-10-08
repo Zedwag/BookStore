bcfb693ea918b428eee9d554313b4a0040b5d062
Zedwag <zedwag@gmail.com>
2019-10-08 03:26:51 +0300

    Updates

---
+++ precache-manifest.f5fa2a1cb92ace7012504d76de1f829c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b48112446f0af08b80a4e30844577ae",
+    "url": "/BookStore/index.html"
+  },
+  {
+    "revision": "20b9a5b1c1c35bb275c8",
+    "url": "/BookStore/static/css/main.121edc02.chunk.css"
+  },
+  {
+    "revision": "121b798439f33c201147",
+    "url": "/BookStore/static/js/2.d9108483.chunk.js"
+  },
+  {
+    "revision": "20b9a5b1c1c35bb275c8",
+    "url": "/BookStore/static/js/main.d5ac23f6.chunk.js"
+  },
+  {
+    "revision": "aa10a2a3e4c65846b056",
+    "url": "/BookStore/static/js/runtime-main.27ab0479.js"
+  },
+  {
+    "revision": "138b0508c14fabc55a20cbb96efa75d8",
+    "url": "/BookStore/static/media/oops.138b0508.png"
+  }
+]);
